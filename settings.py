1b6002032f981115212279f57a91ec176d8fa264
张旭 <zhangxu3486432@gmail.com>
2020-02-04 23:52:28 +0800

    init; 脱敏

---
+++ settings.py
@@ -0,0 +1,32 @@
+#! /usr/bin/env python3
+# -*- coding: utf-8 -*-
+# @Author  : 张旭
+# @Email   : zhangxu3486432@gmail.com
+# @Blog    : https://zhangxu3486432.github.io
+# @FileName: settings.py
+# @Time    : 2020/2/4
+
+# 需要选择的课程
+# COURSES = ['自然语言处理', '强化学习', '生活中的心理学']
+COURSES = []
+
+# 课程所属的学院
+# COLLEGES = ['人工智能学院', '心理学系']
+COLLEGES = []
+
+# 发送选课请求的时间间隔
+TAKE_COURSES_DELAY = 0.1
+
+# 监听成绩的时间间隔
+MONITOR_GRADES_DELAY = 100
+
+# http://sep.ucas.ac.cn/ 教务的账号和密码
+USERNAME = ''
+PASSWORD = ''
+
+
+# SMTP 的账号和授权码，已经接收邮件的邮箱
+SEND_EMAIL = ''
+SEND_EMAIL_PWD = ''
+
+RECEIVE_EMAIL = ''
